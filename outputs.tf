eab5f8aed99cd7abc4d3bb65a405b870a8bd4ac7
Daniel Semedo <34478209+dansemedo@users.noreply.github.com>
2025-08-11 09:02:26 -0300

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+# Outputs for EC2 Module
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
+
+output "public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_eip.app_eip.public_ip
+}
+
+output "private_ip" {
+  description = "Private IP address of the EC2 instance"
+  value       = aws_instance.app_server.private_ip
+}
